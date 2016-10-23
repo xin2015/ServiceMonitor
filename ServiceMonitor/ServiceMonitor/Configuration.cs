136fbece69df40f38b360a175a7ef55387f1ed0f
LiuHaixin <liuhaixin2016@outlook.com>
2016-10-23 12:06:20 +0800

    Init

---
+++ ServiceMonitor/ServiceMonitor/Configuration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceMonitor
+{
+    class Configuration
+    {
+        public static string Description { get; private set; }
+        public static string DisplayName { get; private set; }
+        public static string ServiceName { get; private set; }
+        public static string ServiceMonitorJobCronExpression { get; private set; }
+        public static string MeteorologyDataService { get; private set; }
+        public static string MailHost { get; private set; }
+        public static string MailUsername { get; private set; }
+        public static string MailPassword { get; private set; }
+        public static string MailTo { get; private set; }
+
+        static Configuration()
+        {
+            Description = ConfigurationManager.AppSettings["Description"];
+            DisplayName = ConfigurationManager.AppSettings["DisplayName"];
+            ServiceName = ConfigurationManager.AppSettings["ServiceName"];
+            ServiceMonitorJobCronExpression = ConfigurationManager.AppSettings["ServiceMonitorJobCronExpression"];
+            MeteorologyDataService = ConfigurationManager.AppSettings["MeteorologyDataService"];
+            MailHost = ConfigurationManager.AppSettings["MailHost"];
+            MailUsername = ConfigurationManager.AppSettings["MailUsername"];
+            MailPassword = ConfigurationManager.AppSettings["MailPassword"];
+            MailTo = ConfigurationManager.AppSettings["MailTo"];
+
+            string defaultService = "CustomService";
+            Description = string.IsNullOrWhiteSpace(Description) ? defaultService : Description;
+            DisplayName = string.IsNullOrWhiteSpace(DisplayName) ? defaultService : DisplayName;
+            ServiceName = string.IsNullOrWhiteSpace(ServiceName) ? defaultService : ServiceName;
+
+            string defaultExpression = "0 0 * * * ?";
+            ServiceMonitorJobCronExpression = string.IsNullOrWhiteSpace(ServiceMonitorJobCronExpression) ? defaultExpression : ServiceMonitorJobCronExpression;
+
+            MeteorologyDataService = string.IsNullOrWhiteSpace(MeteorologyDataService) ? "MeteorologyData" : MeteorologyDataService;
+            MailHost = string.IsNullOrWhiteSpace(MailHost) ? "smtp.126.com" : MailHost;
+            MailUsername = string.IsNullOrWhiteSpace(MailUsername) ? "zzfxdhz@126.com" : MailUsername;
+            MailPassword = string.IsNullOrWhiteSpace(MailPassword) ? "ms5lmxin" : MailPassword;
+            MailTo = string.IsNullOrWhiteSpace(MailTo) ? "564030525@qq.com" : MailTo;
+        }
+    }
+}
---
+++ ServiceMonitor/ServiceMonitor/ServiceMonitorJob.cs
@@ -0,0 +1,71 @@
+﻿using Common.Logging;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceMonitor
+{
+    class ServiceMonitorJob : IJob
+    {
+        private static ILog logger;
+        public static string CronExpression { get; set; }
+
+        public static string MonitorServiceName { get; set; }
+
+        static ServiceMonitorJob()
+        {
+            logger = LogManager.GetLogger<ServiceMonitorJob>();
+            CronExpression = Configuration.ServiceMonitorJobCronExpression;
+            MonitorServiceName = Configuration.MeteorologyDataService;
+        }
+        public void Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                ServiceController sc = ServiceController.GetServices().FirstOrDefault(o => o.ServiceName == MonitorServiceName);
+                if (sc != null)
+                {
+                    if (sc.Status == ServiceControllerStatus.Stopped)
+                    {
+                        try
+                        {
+                            sc.Start();
+                            logger.InfoFormat("服务{0}启动成功。", sc.ServiceName);
+                        }
+                        catch (Exception e)
+                        {
+                            string subject = string.Format("服务{0}启动失败。", sc.ServiceName);
+                            SmtpHelper.Default.Send(subject, e.Message, Configuration.MailTo.Split(','));
+                            logger.Error(subject, e);
+                        }
+                    }
+                    else
+                    {
+                        logger.InfoFormat("服务{0}正常运行。", sc.ServiceName);
+                    }
+                }
+                else
+                {
+                    logger.WarnFormat("服务{0}不存在，请检查服务配置。", MonitorServiceName);
+                }
+                try
+                {
+                    string[] a = new string[] { "" };
+                    string b = a[2];
+                }
+                catch (Exception e)
+                {
+                    SmtpHelper.Default.Send("仅测试邮件发送功能，请忽略", e.Message, Configuration.MailTo.Split(','));
+                }
+            }
+            catch (Exception e)
+            {
+                logger.Error("监控服务出错。", e);
+            }
+        }
+    }
+}
